3ea99eb614d56e34623f9aeaa3fb6ec144262cae
Rect <161033689+riaton@users.noreply.github.com>
2025-10-22 14:41:17 +0000

    strands実装追加

--- strands/whatsnew.py
+++ strands/whatsnew.py
@@ -1,8 +1,11 @@
-import feedparser
+import os, feedparser, asyncio
+import streamlit as st
 from strands import Agent, tool
-from dotenv import load_dotenv
 
-load_dotenv()
+
+os.environ["AWS_ACCESS_KEY_ID"] = st.secrets["AWS_ACCESS_KEY_ID"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets["AWS_SECRET_ACCESS_KEY"]
+os.environ["AWS_REGION"] = st.secrets["AWS_REGION"]
 
 @tool
 def get_aws_updates(service_name: str) -> list:
@@ -20,3 +23,43 @@ def get_aws_updates(service_name: str) -> list:
                 break
 
     return results
+
+agent = Agent(
+    model = "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    tools = [get_aws_updates] 
+)
+
+st.title("AWSアップデート確認くん")
+service_name = st.text_input("アップデートを知りたいAWSサービス名を入力してください")
+
+async def process_stream(service_name, container):
+    text_holder = container.empty()
+    response = ""
+    prompt = f"AWSの{service_name.strip()}の最新のアップデートを、日付付きで要約して。"
+    
+    async for chunk in agent.stream_async(prompt):
+        if not isinstance(chunk, dict):
+            continue
+
+        event = chunk.get("event")
+
+        if isinstance(event, dict) and "contentBlockStart" in event:
+            tool_use = event["contentBlockStart"].get("start", {}).get("toolUse", {})
+            tool_name = tool_use.get("name")
+
+            if response:
+                text_holder.markdown(response)
+                response = ""
+            
+            container.info(f"ツール実行中: {tool_name}")
+            text_holder = container.empty()
+        
+        if text := chunk.get("data"):
+            response += text
+            text_holder.markdown(response)
+
+if st.button("確認"):
+    if service_name:
+        with st.spinner("AWSアップデートを確認中..."):
+            container = st.container()
+            asyncio.run(process_stream(service_name, container))
